a01fffaf2cef8e1c828fe73f30aa03edae71ba20
Christopher Perez <chrisperez0531@gmail.com>
2023-09-26 22:13:30 -0400

    handling signup error

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -6,16 +6,22 @@ import signupImage from "../Assets/signupNetflix.jpg";
 const Signup = () => {
   const [email, SetEmail] = useState("");
   const [password, SetPassword] = useState("");
+  const [error, SetError] = useState(""); 
   const { user, signUp } = UserAuth();
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    if (password.length < 6) {
+      SetError("Password should be at least 6 characters long."); // Step 2: Set error state
+      return;
+    }
     try {
       await signUp(email, password);
-      navigate('/')
+      navigate('/');
     } catch (error) {
       console.log(error);
+      SetError("There was an issue signing up. Please try again.");
     }
   };
 
@@ -32,6 +38,7 @@ const Signup = () => {
           <div className="max-w-[450px] h-[600px] mx-auto bg-black/75 text-white">
             <div className="max-w-[320px] mx-auto py-16">
               <h1 className="text-3xl font-bold">Sign Up</h1>
+              {error && <p className="text-red-500 mt-2">{error}</p>} {/* Step 3: Display error message */}
               <form
                 onSubmit={handleSubmit}
                 className="w-full flex flex-col py-4"
